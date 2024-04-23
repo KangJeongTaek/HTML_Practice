1031fd6f5111eae4134238c4e4746489349f15a1
KangJeongTaek <dfgddjd@naver.com>
2024-04-23 14:57:55 +0900

    최초 커밋

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const searchConsole = document.getElementById("search_text");
+
+function availabilityFunc(){
+    recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+    recognition.lang = "ko";
+    recognition.maxAlternativecs =5;
+
+    if (!recognition){
+        alert("현재 브라우저는 불가능");
+    }
+}
+
+function readText(){
+    const searchText = document.getElementById("search_text").value;
+
+    if(!searchText) return;
+    const utter = new SpeechSynthesisUtterance(searchText);
+
+    const lang = document.getElementById("lang").value;
+    
+    utter.lang = lang;
+
+    window.speechSynthesis.speak(utter);
+}
+
+window.addEventListener("load",availabilityFunc);
